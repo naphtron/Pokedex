23826945214a96130f2508fc04e8e938de8c5988
naphtron <naphtron@yahoo.com>
2025-11-22 23:10:20 +0300

    Added Pokemon Type

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,19 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
 import { useEffect, useState } from "react";
 
 
 interface Pokemon {
   name: string;
-  url:string;
+  image:string;
+  imageBack: string;
+  types: PokemonType[];
+}
+
+interface PokemonType {
+  type: {
+    name: string;
+    url: string;
+  }
 }
 
 export default function Index() {
@@ -17,10 +26,24 @@ export default function Index() {
 
   async function fetchPokemons (){
     try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
       const data = await response.json();
-      setPokemons(data.results);
-      console.log(data);
+
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+            imageBack: details.sprites.back_default,
+            // You can add more detailed info here if needed
+          };
+        })
+      )
+
+      setPokemons(detailedPokemons);
+      console.log("Det: ",detailedPokemons);
     }
     catch (error) {
       console.error('Error fetching Pokémon data:', error);
@@ -40,6 +63,16 @@ export default function Index() {
       {pokemons.map((pokemon:Pokemon)=>(
         <View key={pokemon.name}>
           <Text>{pokemon.name}</Text>
+          <View style={{flexDirection: 'row'}}>
+          <Image
+            source={{uri: pokemon.image}}
+            style={{width: 150, height: 150}}
+          />
+          <Image
+            source={{uri: pokemon.imageBack}}
+            style={{width: 150, height: 150}}
+          />
+          </View>
         </View>
       ))}
     </ScrollView>
